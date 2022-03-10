5598f5aa034b853b3cbfce0cc1489a4211e8b3a8
Sochaj Yoann <yoann.sochaj@etu.unilim.fr>
2022-03-10 16:01:13 +0100

    SommeNulV1

--- TheorieProject/jeu.py
+++ TheorieProject/jeu.py
@@ -3,3 +3,14 @@ class Jeu:
         self.joeurs=joueurs
         self.jeu=jeu
     
+    def estSommeNul(self):
+        matrice= [[[-2,2],[-3,3],[-2,2]],[[-2,2],[-5,5],[-2,2]],[[-4,4],[-7,7],[0,0]]]
+        for i in range(0, len(matrice)):
+            for j in range(0, len(matrice[i])):
+                somme=0
+                for strategie in range (0,len(matrice[i][j])):
+                    somme+=matrice[i][j][strategie]
+                if(somme!=0):
+                    return False
+        return True
+
